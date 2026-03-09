56fc613c3a1e68bb158984effb78a91c4ca68a2c
viperop01 <agamhora481@gmail.com>
2026-03-09 15:30:27 +0530

    UC1: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 class PalindromeCheckerApp {
 
@@ -22,6 +23,51 @@ class PalindromeCheckerApp {
             System.out.println("It is NOT a Palindrome");
         }
 
+        // -------- UC4 Character Array Based Palindrome Check --------
+
+        char[] arr = input.toCharArray();
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome (Checked using Character Array)");
+        } else {
+            System.out.println("Not a Palindrome (Checked using Character Array)");
+        }
+
+        // -------- UC5 Stack Based Palindrome Check --------
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String stackReverse = "";
+
+        while (!stack.isEmpty()) {
+            stackReverse = stackReverse + stack.pop();
+        }
+
+        if (input.equalsIgnoreCase(stackReverse)) {
+            System.out.println("Palindrome (Checked using Stack)");
+        } else {
+            System.out.println("Not a Palindrome (Checked using Stack)");
+        }
+
         sc.close();// test commit
     }
+}
 }
